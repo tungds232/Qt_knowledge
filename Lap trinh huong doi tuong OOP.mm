64c57efa593e7f210a3e47525617222ac8d66ef0
dstung <tungds232@gmail.com>
2018-10-15 13:41:31 +0700

    181015_update Design pattern
    Signed-off-by: dstung <tungds232@gmail.com>

--- Lap trinh huong doi tuong OOP.mm
+++ Lap trinh huong doi tuong OOP.mm
@@ -2,5 +2,370 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1539446202525" ID="ID_1143506264" MODIFIED="1539446210760" TEXT="Lap trinh huong doi tuong OOP">
 <node CREATED="1539446687263" ID="ID_332647996" MODIFIED="1539446695858" POSITION="right" TEXT="L&#x1ead;p tr&#xec;nh chia l&#xe0;m c&#xe1;c &#x111;&#x1ed1;i t&#x1b0;&#x1ee3;ng &#x111;&#x1ec3; x&#x1eed; l&#xfd;"/>
+<node CREATED="1539567157380" ID="ID_1215217540" MODIFIED="1539567164036" POSITION="right" TEXT="&#x1af;u &#x111;i&#x1ec3;m">
+<node CREATED="1539567165419" ID="ID_572382782" MODIFIED="1539567189818" TEXT="Kh&#x1eaf;c ph&#x1ee5;c y&#x1ebf;u &#x111;i&#x1ec3;m c&#x1ee7;a PPLT tr&#x1b0;&#x1edb;c &#x111;&#xf3;"/>
+<node CREATED="1539567192179" ID="ID_1790736206" MODIFIED="1539567210986" TEXT="D&#x1ec5; d&#xe0;ng qu&#x1ea3;n l&#xfd; code khi c&#xf3; s&#x1ef1; thay &#x111;&#x1ed5;i ch&#x1b0;&#x1a1;ng tr&#xec;nh"/>
+<node CREATED="1539567211356" ID="ID_1449667141" MODIFIED="1539567217014" TEXT="D&#x1ec5; m&#x1edf; r&#x1ed9;ng d&#x1ef1; &#xe1;n"/>
+<node CREATED="1539567219547" ID="ID_604260479" MODIFIED="1539567231043" TEXT="Ti&#x1ebf;t ki&#x1ec7;m t&#xe0;i nguy&#xea;n &#x111;&#xe1;ng k&#x1ec3; cho h&#x1ec7; th&#x1ed1;ng"/>
+<node CREATED="1539567231459" ID="ID_928969185" MODIFIED="1539567239288" TEXT="C&#xf3; t&#xed;nh b&#x1ea3;o m&#x1ead;t cao"/>
+<node CREATED="1539567239821" ID="ID_965847973" MODIFIED="1539567245556" TEXT="T&#xed;nh t&#xe1;i s&#x1eed; d&#x1ee5;ng cao"/>
+</node>
+<node CREATED="1539567327388" ID="ID_1060754070" MODIFIED="1539567338371" POSITION="right" TEXT="&#x110;&#x1ecb;nh ngh&#x129;a c&#x1a1; b&#x1ea3;n">
+<node CREATED="1539567338955" ID="ID_39617562" MODIFIED="1539567342079" TEXT="&#x110;&#x1ed1;i t&#x1b0;&#x1ee3;ng">
+<node CREATED="1539567350594" ID="ID_69504743" MODIFIED="1539567362106" TEXT="Thu&#x1ed9;c t&#xed;nh (attributes)"/>
+<node CREATED="1539567362930" ID="ID_995106806" MODIFIED="1539567367569" TEXT="H&#xe0;nh &#x111;&#x1ed9;ng (methods)"/>
+<node CREATED="1539567370003" ID="ID_1813299943" MODIFIED="1539567382066" TEXT="vd: &#x110;i&#x1ec7;n tho&#x1ea1;i, b&#xec;nh n&#x1b0;&#x1edb;c, nh&#xe2;n v&#x1ead;t &#x1ea3;o"/>
+</node>
+</node>
+<node CREATED="1539567907658" ID="ID_307223305" MODIFIED="1539567910625" POSITION="right" TEXT="4 t&#xed;nh ch&#x1ea5;t">
+<node CREATED="1539567911178" ID="ID_91006504" MODIFIED="1539567917923" TEXT="Abstraction: Tr&#x1eeb;u t&#x1b0;&#x1ee3;ng">
+<node CREATED="1539567918459" ID="ID_517049647" MODIFIED="1539567934459" TEXT="T&#x1ead;p trung v&#xe0;o c&#x1ed1;t l&#xf5;i c&#x1ee7;a &#x111;&#x1ed1;i t&#x1b0;&#x1ee3;ng"/>
+<node CREATED="1539573161922" ID="ID_1707424645" MODIFIED="1539573181258" TEXT="&#xec;C&#xf3; th&#x1ec3; khai b&#xe1;o m&#xe0; kh&#xf4;ng quan t&#xe2;m b&#xea;n trong c&#xf3; g"/>
+</node>
+<node CREATED="1539567939267" ID="ID_116254102" MODIFIED="1539567995713" TEXT="Encapsulation: &#x110;&#xf3;ng g&#xf3;i">
+<node CREATED="1539567946258" ID="ID_768228628" MODIFIED="1539567974980" TEXT="Ch&#x1ec9; h&#xe0;m th&#xe0;nh vi&#xea;n c&#x1ee7;a &#x111;&#x1ed1;i t&#x1b0;&#x1ee3;ng m&#x1edb;i c&#xf3; quy&#x1ec1;n thay &#x111;&#x1ed5;i d&#x1eef; li&#x1ec7;u n&#x1ed9;i t&#x1ea1;i"/>
+<node CREATED="1539567976066" ID="ID_614839297" MODIFIED="1539567987125" TEXT="Ng&#x1b0;&#x1edd;i d&#xf9;ng ho&#x1eb7;c &#x111;&#x1ed1;i t&#x1b0;&#x1ee3;ng kh&#xe1;c kh&#xf4;ng th&#x1ec3;"/>
+</node>
+<node CREATED="1539567996698" ID="ID_695525878" MODIFIED="1539568003873" TEXT="Inheritance: K&#x1ebf; th&#x1eeb;a">
+<node CREATED="1539568004538" ID="ID_585281811" MODIFIED="1539568021098" TEXT="K&#x1ebf; th&#x1eeb;a, t&#xe1;i s&#x1eed; d&#x1ee5;ng method,atribute c&#x1ee7;a l&#x1edb;p c&#x1a1; s&#x1edf;"/>
+<node CREATED="1539568027746" ID="ID_1690598588" MODIFIED="1539568039779" TEXT="Class con c&#xf3; th&#x1ec3; k&#x1ebf; th&#x1eeb;a class cha c&#xf3; v&#xe0; cho ph&#xe9;p"/>
+</node>
+<node CREATED="1539568040938" ID="ID_385320031" MODIFIED="1539568047480" TEXT="Polymorphism: &#x110;a h&#xec;nh">
+<node CREATED="1539568055602" ID="ID_541016088" MODIFIED="1539568071683" TEXT="Cho ph&#xe9;p method kh&#xe1;c nhau th&#x1ef1;c thi kh&#xe1;c nhau tr&#xea;n c&#xe1;c &#x111;&#x1ed1;i t&#x1b0;&#x1ee3;ng kh&#xe1;c nhau"/>
+</node>
+</node>
+<node CREATED="1539568301667" ID="ID_405387686" MODIFIED="1539568327738" POSITION="right" TEXT="Sample khai b&#xe1;o">
+<node CREATED="1539568319330" MODIFIED="1539568319330" TEXT="class ClassName{">
+<node CREATED="1539568319332" MODIFIED="1539568319332" TEXT="// Khai b&#xe1;o thu&#x1ed9;c t&#xed;nh: gi&#x1ed1;ng khai b&#xe1;o bi&#x1ebf;n"/>
+<node CREATED="1539568319333" MODIFIED="1539568319333" TEXT="// Khai b&#xe1;o ph&#x1b0;&#x1a1;ng th&#x1ee9;c: Gi&#x1ed1;ng khai b&#xe1;o h&#xe0;m"/>
+<node CREATED="1539568319334" MODIFIED="1539568319334" TEXT="// private, protected, public"/>
+<node CREATED="1539568319334" MODIFIED="1539568319334" TEXT="// private: Ch&#x1ec9; s&#x1eed; d&#x1ee5;ng trong class"/>
+<node CREATED="1539568319335" MODIFIED="1539568319335" TEXT="// public: B&#xea;n ngo&#xe0;i c&#xf3; th&#x1ec3; s&#x1eed; d&#x1ee5;ng &#x111;&#x1b0;&#x1ee3;c."/>
+</node>
+<node CREATED="1539568326610" MODIFIED="1539568326610" TEXT="ClassName cl; // C&#xe1;ch kh&#x1a1;i t&#x1ea1;o 1 &#x111;&#x1ed1;i t&#x1b0;&#x1ee3;ng"/>
+<node CREATED="1539568326610" MODIFIED="1539568326610" TEXT="cl.address; // C&#xe1;ch truy xu&#x1ea5;t &#x111;&#x1ebf;n 1 thu&#x1ed9;c t&#xed;nh c&#x1ee7;a 1 class"/>
+<node CREATED="1539568326610" ID="ID_1618341133" MODIFIED="1539568326610" TEXT="cl.input(); // C&#xe1;ch truye xu&#x1ea5;t &#x111;&#x1ebf;n 1 ph&#x1b0;&#x1a1;ng th&#x1ee9;c c&#x1ee7;a 1 class"/>
+</node>
+<node CREATED="1539568378931" ID="ID_1480504647" MODIFIED="1539568381798" POSITION="right" TEXT="&#x110;&#x1ecb;nh ngh&#x129;a method">
+<node CREATED="1539568382366" ID="ID_1629635134" MODIFIED="1539568577748" TEXT="C&#xe1;ch 1: Trong class">
+<node CREATED="1539568406053" ID="ID_53888009" MODIFIED="1539568534979" TEXT="Class a { public: void Nhap(){..............} }"/>
+</node>
+<node CREATED="1539568447427" ID="ID_1851739787" MODIFIED="1539568464337" TEXT="C&#xe1;ch 2: Ngo&#xe0;i class">
+<node CREATED="1539568544547" ID="ID_1229383219" MODIFIED="1539568553738" TEXT="Khai b&#xe1;o trong class">
+<node CREATED="1539568516115" ID="ID_907458893" MODIFIED="1539568531594" TEXT="Class a { public: void Xuat(); }"/>
+</node>
+<node CREATED="1539568479808" ID="ID_1667150162" MODIFIED="1539568622570" TEXT="&#x110;&#x1ecb;nh ngh&#x129;a b&#xea;n ngo&#xe0;i nh&#x1b0;ng v&#x1eab;n trong class">
+<node CREATED="1539568570267" ID="ID_666963741" MODIFIED="1539568570267" TEXT="void HocSinh::Xuat(){}"/>
+</node>
+</node>
+</node>
+<node CREATED="1539568637075" ID="ID_1628617994" MODIFIED="1539568641852" POSITION="right" TEXT="Con tr&#x1ecf; this">
+<node CREATED="1539568856627" ID="ID_1324694860" MODIFIED="1539568864554" TEXT="Tham chi&#x1ebf;u &#x111;&#x1ebf;n &#x111;&#x1ed1;i t&#x1b0;&#x1ee3;ng g&#x1ecd;i h&#xe0;m th&#xe0;nh ph&#x1ea7;n">
+<node CREATED="1539568866202" ID="ID_1170523668" MODIFIED="1539568879028" TEXT="C&#xe1;ch d&#xf9;ng 1: &#xed;t d&#xf9;ng">
+<node CREATED="1539568884667" ID="ID_1541413111" MODIFIED="1539568902842" TEXT="cin &gt;&gt; Tuso;"/>
+<node CREATED="1539568889016" ID="ID_1585256363" MODIFIED="1539568900331" TEXT="cin &gt;&gt; this -&gt; Tuso;"/>
+</node>
+<node CREATED="1539568904010" ID="ID_1675151046" MODIFIED="1539568923126" TEXT="C&#xe1;ch 2: tr&#x1ea3; v&#x1ec1; object hi&#x1ec7;n t&#x1ea1;i">
+<node CREATED="1539568927082" ID="ID_783872262" MODIFIED="1539568940364" TEXT="class a"/>
+<node CREATED="1539568941771" ID="ID_1343394889" MODIFIED="1539569120165">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      a a::rutGon(){
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;this-&gt;attribute1 = 12;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;....
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;return *this;
+    </p>
+    <p>
+      }
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1539569006458" ID="ID_177775789" MODIFIED="1539569027333">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      a tung;
+    </p>
+    <p>
+      a.rutGon();
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1539567099460" ID="ID_1805685403" MODIFIED="1539567100944" POSITION="right" TEXT="Link">
+<node CREATED="1539567101396" LINK="https://toidicode.com/lap-trinh-huong-doi-tuong-la-gi-va-uu-diem-cua-no-97.html" MODIFIED="1539567101396" TEXT="https://toidicode.com/lap-trinh-huong-doi-tuong-la-gi-va-uu-diem-cua-no-97.html"/>
+</node>
+<node CREATED="1539569171643" ID="ID_1008518008" MODIFIED="1539569234273" POSITION="left" TEXT="Constructor">
+<node CREATED="1539569183315" ID="ID_433011932" MODIFIED="1539569394282" TEXT="H&#xe0;m &#x111;&#x1eb7;c bi&#x1ec7;t &#x111;&#x1ec3; kh&#x1edf;i t&#x1ea1;o ban &#x111;&#x1ea7;u cho Object">
+<node CREATED="1539569394760" ID="ID_1796812469" MODIFIED="1539569405891" TEXT="Vd mu&#x1ed1;n c&#xe1;c bi&#x1ebf;n private &#x111;&#x1b0;&#x1ee3;c g&#xe1;n 1 gi&#xe1; tr&#x1ecb; kh&#x1edf;i t&#x1ea1;o"/>
+<node CREATED="1539569410306" ID="ID_1787417067" MODIFIED="1539569420773" TEXT="Ho&#x1eb7;c khi mu&#x1ed1;n kh&#x1edf;i t&#x1ea1;o m&#xe0; ch&#x1b0;a bi&#x1ebf;t chi ti&#x1ebf;t"/>
+</node>
+<node CREATED="1539569192282" ID="ID_905033287" MODIFIED="1539569197401" TEXT="C&#xf9;ng t&#xea;n v&#x1edb;i Object"/>
+<node CREATED="1539569223562" ID="ID_81206450" MODIFIED="1539569225713" TEXT="Ph&#xe2;n lo&#x1ea1;i">
+<node CREATED="1539569226161" ID="ID_1904834501" MODIFIED="1539569239571" TEXT="Constructor m&#x1eb7;c &#x111;&#x1ecb;nh">
+<node CREATED="1539569430146" ID="ID_1419246006" MODIFIED="1539569435010" TEXT="Kh&#xf4;ng c&#xf3; ki&#x1ec3;u tr&#x1ea3; v&#x1ec1;"/>
+<node CREATED="1539569435457" ID="ID_90892617" MODIFIED="1539569440209" TEXT="C&#xf9;ng t&#xea;n v&#x1edb;i class"/>
+<node CREATED="1539569443010" ID="ID_618773080" MODIFIED="1539569446930" TEXT="N&#x1eb1;m trong public"/>
+<node CREATED="1539569449026" ID="ID_1157163752" MODIFIED="1539569450733" TEXT="Vd:">
+<node CREATED="1539569451184" ID="ID_1161004564" MODIFIED="1539571338241">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      class hoc{
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;private:
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;int a;&#160;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;int b;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;public:
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;hoc(){
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;a = 0;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;}
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1539569240122" ID="ID_696352248" MODIFIED="1539569244177" TEXT="Constructor c&#xf3; tham s&#x1ed1;">
+<node CREATED="1539569542042" ID="ID_1999227909" MODIFIED="1539569547889" TEXT="Constructor m&#x1eb7;c &#x111;&#x1ecb;nh"/>
+<node CREATED="1539569548906" ID="ID_142155098" MODIFIED="1539569552565" TEXT="C&#xf3; tham s&#x1ed1; truy&#x1ec1;n v&#xe0;o">
+<node CREATED="1539569562187" ID="ID_517368841" MODIFIED="1539569586271">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      hoc (int so1, int so2){
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;a = so1;
+    </p>
+    <p>
+      }
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1539569597475" ID="ID_672055662" MODIFIED="1539569603907" TEXT="G&#x1ecd;i s&#x1eed; d&#x1ee5;ng">
+<node CREATED="1539569604451" ID="ID_430376565" MODIFIED="1539569612521" TEXT="hoc tung(1,2)"/>
+<node CREATED="1539569612905" ID="ID_966436388" MODIFIED="1539569618785" TEXT="hoc tung = hoc(1,2)"/>
+</node>
+</node>
+<node CREATED="1539569244450" ID="ID_1181037996" MODIFIED="1539569249387" TEXT="Constructor sao ch&#xe9;p">
+<node CREATED="1539571260755" ID="ID_1964759583" MODIFIED="1539571273097" TEXT="G&#xe1;n b&#xec;nh th&#x1b0;&#x1edd;ng">
+<node CREATED="1539571273763" ID="ID_1357543791" MODIFIED="1539571348312">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      hoc tung, minh;
+    </p>
+    <p>
+      tung.a = 15; tung.b = 5;
+    </p>
+    <p>
+      minh = tung;
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1539571349346" ID="ID_948581904" MODIFIED="1539571356672" TEXT="minh.a = 15, minh.b =5"/>
+</node>
+</node>
+<node CREATED="1539571312659" ID="ID_1398755209" MODIFIED="1539571327226" TEXT="G&#xe1;n tham chi&#x1ebf;u, &#x111;&#x1ec3; sao ch&#xe9;p 1 ph&#x1ea7;n th&#xf4;i">
+<node CREATED="1539571362699" ID="ID_528935688" MODIFIED="1539571420879">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Trong khai bao class
+    </p>
+    <p>
+      public:
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;hoc (const hoc &amp;name){
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;a = name.a;
+    </p>
+    <p>
+      &#160;&#160;&#160;}
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1539571421843" ID="ID_426252" MODIFIED="1539571447225" TEXT="hoc minh(tung);"/>
+</node>
+</node>
+</node>
+<node CREATED="1539569662778" ID="ID_1973912336" MODIFIED="1539569664661" TEXT="Tham chi&#x1ebf;u">
+<node CREATED="1539569665160" ID="ID_1725678690" MODIFIED="1539569671482" TEXT="Nick name, d&#xf9;ng nh&#x1b0; bi&#x1ebf;n"/>
+<node CREATED="1539569671787" ID="ID_1691596820" MODIFIED="1539569673434" TEXT="vd">
+<node CREATED="1539569674059" ID="ID_1603624322" MODIFIED="1539571057611">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      int b = 5;
+    </p>
+    <p>
+      int &amp;c = b;
+    </p>
+    <p>
+      cout &lt;&lt; &quot;Gia tri tham chieu b: &quot; &lt;&lt;&#160;c &lt;&lt; endl;
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1539571058971" ID="ID_1851248594" MODIFIED="1539571062477" TEXT="c = 5"/>
+</node>
+<node CREATED="1539569688555" ID="ID_1941614337" MODIFIED="1539569693084" TEXT="c l&#xe0; tham chi&#x1ebf;u c&#x1ee7;a b"/>
+</node>
+</node>
+</node>
+<node CREATED="1539571460051" ID="ID_932115954" MODIFIED="1539571462281" POSITION="left" TEXT="Destructor">
+<node CREATED="1539571493530" ID="ID_1503332788" MODIFIED="1539571502979" TEXT="C&#xf9;ng t&#xea;n v&#x1edb;i class nh&#x1b0;ng c&#xf3; ~"/>
+<node CREATED="1539571506595" ID="ID_668637603" MODIFIED="1539571517530" TEXT="&#x110;&#x1b0;&#x1ee3;c t&#x1ef1; &#x111;&#x1ed9;ng th&#x1ef1;c hi&#x1ec7;n khi &#x111;&#x1ed1;i t&#x1b0;&#x1ee3;ng h&#x1ebf;t ph&#x1ea1;m vi s&#x1eed; d&#x1ee5;ng"/>
+<node CREATED="1539571519162" ID="ID_1926328137" MODIFIED="1539571521672" TEXT="Thu&#x1ed9;c t&#xed;nh public"/>
+<node CREATED="1539571577651" ID="ID_895941881" MODIFIED="1539571595745" TEXT="Class d&#xf9;ng con tr&#x1ecf;, d&#xf9;ng destructor &#x111;&#x1ec3; gi&#x1ea3;i ph&#xf3;ng"/>
+<node CREATED="1539571532107" ID="ID_862243727" MODIFIED="1539571533144" TEXT="Vd">
+<node CREATED="1539571533642" ID="ID_1284936427" MODIFIED="1539571682048">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      private:
+    </p>
+    <p>
+      &#160;&#160;&#160;int *p;
+    </p>
+    <p>
+      pubilc:
+    </p>
+    <p>
+      ~hoc(){
+    </p>
+    <p>
+      &#160;&#160;&#160;cout &lt;&lt; &quot;Day la ham destructor&quot;;<br />&#160;&#160; if (p) {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;&#160;delete p;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;&#160;p = nullptr;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;}
+    </p>
+    <p>
+      }
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1539571993218" ID="ID_1593327715" MODIFIED="1539572033100" POSITION="left" TEXT="static data member">
+<node CREATED="1539571995106" ID="ID_1181113596" MODIFIED="1539572020067" TEXT="attribute ho&#x1eb7;c method d&#xf9;ng chung cho t&#x1ea5;t c&#x1ea3; c&#xe1;c object t&#x1ea1;o ra t&#x1eeb; class"/>
+<node CREATED="1539572044298" ID="ID_436725886" MODIFIED="1539572062437" TEXT="Khai b&#xe1;o trong class, &#x111;&#x1ecb;nh ngh&#x129;a ngo&#xe0;i class"/>
+<node CREATED="1539572063505" ID="ID_650419249" MODIFIED="1539572074242" TEXT="Gi&#xe1; tr&#x1ecb; kh&#x1edf;i t&#x1ea1;o = 0, c&#xf3; th&#x1ec3; &#x111;&#x1ecb;nh ngh&#x129;a l&#x1ea1;i"/>
+<node CREATED="1539572100547" ID="ID_1590449444" MODIFIED="1539572101548" TEXT="Vd">
+<node CREATED="1539572102258" ID="ID_1777060243" MODIFIED="1539572154622">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      class hoc () {
+    </p>
+    <p>
+      private:
+    </p>
+    <p>
+      &#160;&#160;&#160;static int count;
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      int hoc::count;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1539572157275" ID="ID_1297381842" MODIFIED="1539572162874" POSITION="left" TEXT="satic member functions">
+<node CREATED="1539572176458" ID="ID_1236863984" MODIFIED="1539572197240" TEXT="C&#xf3; th&#x1ec3; truy c&#x1ead;p static data ho&#x1eb7;c static method kh&#xe1;c"/>
+<node CREATED="1539572233738" ID="ID_1153901943" MODIFIED="1539572234672" TEXT="Vd">
+<node CREATED="1539572235938" ID="ID_680259416" MODIFIED="1539572294735">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      public:
+    </p>
+    <p>
+      &#160;&#160;&#160;static int countValue(){
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;return count;
+    </p>
+    <p>
+      &#160;&#160;&#160;}
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
 </node>
 </map>
