a820dbd7cfcbfc86939d69e49011c8482c2bf24f
tungds232 <44081451+tungds232@users.noreply.github.com>
2018-10-15 07:28:25 +0700

    Add files via upload

---
+++ Design Pattern.mm
@@ -0,0 +1,64 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1539444586090" ID="ID_15885761" MODIFIED="1539444590166" TEXT="Design Pattern">
+<node CREATED="1539444602611" ID="ID_316939663" MODIFIED="1539444606284" POSITION="right" TEXT="Intro">
+<node CREATED="1539444607124" ID="ID_742880641" MODIFIED="1539444616313" TEXT="Gi&#x1ea3;i ph&#xe1;p t&#x1ed5;ng th&#x1ec3; cho v&#x1ea5;n &#x111;&#x1ec1; thi&#x1ebf;t k&#x1ebf; ph&#x1ea7;n m&#x1ec1;m"/>
+<node CREATED="1539444627528" ID="ID_205427395" MODIFIED="1539444639857" TEXT="M&#xf4; t&#x1ea3; c&#xe1;ch gi&#x1ea3;i quy&#x1ebf;t v&#x1ea5;n &#x111;&#x1ec1;, c&#xf3; th&#x1ec3; d&#xf9;ng trong nhi&#x1ec1;u t&#xec;nh hu&#x1ed1;ng"/>
+<node CREATED="1539444660852" ID="ID_1819963066" MODIFIED="1539444667843" TEXT="OOP">
+<node CREATED="1539444668755" ID="ID_1248254433" MODIFIED="1539444670983" TEXT="S&#x1ef1; t&#x1b0;&#x1a1;ng t&#xe1;c"/>
+<node CREATED="1539444671232" ID="ID_258504295" MODIFIED="1539444674063" TEXT="M&#x1ed1;i quan h&#x1ec7;"/>
+</node>
+<node CREATED="1539444691911" ID="ID_1586859592" MODIFIED="1539444707966" TEXT="Algorithm kh&#xf4;ng &#x111;&#x1b0;&#x1ee3;c coi l&#xe0; Design pattern">
+<node CREATED="1539444714286" ID="ID_825619148" MODIFIED="1539444720075" TEXT="Gi&#x1ea3;i quy&#x1ebf;t v&#x1ea5;n &#x111;&#x1ec1; v&#x1ec1; t&#xed;nh to&#xe1;n"/>
+<node CREATED="1539444720406" ID="ID_591213578" MODIFIED="1539444727187" TEXT="Ko ph&#x1ea3;i v&#x1ea5;n &#x111;&#x1ec1; v&#x1ec1; thi&#x1ebf;t k&#x1ebf;"/>
+</node>
+<node CREATED="1539444735278" ID="ID_1776202496" MODIFIED="1539444742035" TEXT="Gi&#xfa;p thi&#x1ebf;t k&#x1ebf;">
+<node CREATED="1539444742843" ID="ID_221221631" MODIFIED="1539444744475" TEXT="Linh ho&#x1ea1;t"/>
+<node CREATED="1539444746150" ID="ID_961399987" MODIFIED="1539444749311" TEXT="D&#x1ec5; d&#xe0;ng thay &#x111;&#x1ed5;i"/>
+<node CREATED="1539444749549" ID="ID_1922129683" MODIFIED="1539444750954" TEXT="B&#x1ea3;o tr&#xec;"/>
+</node>
+</node>
+<node CREATED="1539444785153" ID="ID_491438441" MODIFIED="1539444787340" POSITION="right" TEXT="Why learn">
+<node CREATED="1539444788376" ID="ID_424683394" MODIFIED="1539444803523" TEXT="Ph&#xe1;t tri&#x1ec3;n ph&#x1ea7;n m&#x1ec1;m d&#x1ec5; d&#xe0;ng h&#x1a1;n">
+<node CREATED="1539444804587" ID="ID_165340879" MODIFIED="1539444812695" TEXT="B&#x1ea5;t bi&#x1ebf;n = s&#x1ef1; thay &#x111;&#x1ed5;i">
+<node CREATED="1539444828347" ID="ID_1607592502" MODIFIED="1539444832959" TEXT="Requirement change"/>
+<node CREATED="1539444833330" ID="ID_611670948" MODIFIED="1539444850623" TEXT="H&#x1ec7; h&#x1ed1;ng ph&#xec;nh to"/>
+<node CREATED="1539444851077" ID="ID_438607984" MODIFIED="1539444866217" TEXT="Optimize performance"/>
+<node CREATED="1539444868752" ID="ID_710844637" MODIFIED="1539444871126" TEXT="T&#xed;nh n&#x103;ng m&#x1edb;i"/>
+</node>
+<node CREATED="1539444882570" ID="ID_484562628" MODIFIED="1539444898220" TEXT="X&#xe2;y d&#x1ef1;ng ph&#x1ea7;n m&#x1ec1;m m&#xe0; &#x1ea3;nh h&#x1b0;&#x1edf;ng c&#x1ee7;a s&#x1ef1; thay &#x111;&#x1ed5;i l&#xe0; nh&#x1ecf; nh&#x1ea5;t">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1539444925078" ID="ID_1647233021" MODIFIED="1539444932381" TEXT="Hi&#x1ec3;u code c&#x169; &#x111;&#x1ec3; dev th&#xea;m ko l&#x1ed7;i"/>
+</node>
+<node CREATED="1539444968516" ID="ID_1455398727" MODIFIED="1539445000767" TEXT="-&gt; Nguy&#xea;n l&#xfd;, thi&#x1ebf;t k&#x1ebf; t&#x1ead;p h&#x1ee3;p l&#x1ea1;i: Design pattern">
+<node CREATED="1539445015420" ID="ID_418289609" MODIFIED="1539445024920" TEXT="Ko ph&#x1ea3;i lib hay module"/>
+<node CREATED="1539445025213" ID="ID_357634458" MODIFIED="1539445052922" TEXT="Guideline t&#xed;ch h&#x1ee3;p v&#xe0;o core c&#x1ee7;a thi&#x1ebf;t k&#x1ebf; &#x111;&#x1ec3; t&#x1ea1;o h&#x1ec7; th&#x1ed1;ng &#x111;&#x1ed1;i t&#x1b0;&#x1ee3;ng">
+<node CREATED="1539445054297" ID="ID_1833945782" MODIFIED="1539445058686" TEXT="Linh ho&#x1ea1;t"/>
+<node CREATED="1539445059238" ID="ID_195519904" MODIFIED="1539445061296" TEXT="D&#x1ec5; b&#x1ea3;o tr&#xec;"/>
+</node>
+</node>
+</node>
+<node CREATED="1539445652337" ID="ID_711274752" MODIFIED="1539445660011" TEXT="C&#x1ea3;i thi&#x1ec7;n k&#x129; n&#x103;ng l&#x1ead;p tr&#xec;nh OOP">
+<node CREATED="1539445667259" ID="ID_527846804" MODIFIED="1539445676380" TEXT="N&#x1ec1;n t&#x1ea3;ng OOP l&#xe0; 4 nguy&#xea;n t&#x1eaf;c">
+<node CREATED="1539445683735" ID="ID_1754557146" MODIFIED="1539445691965" TEXT="T&#xed;nh tr&#x1eeb;u t&#x1b0;&#x1ee3;ng"/>
+<node CREATED="1539445692220" ID="ID_457363525" MODIFIED="1539445695292" TEXT="T&#xed;nh k&#x1ebf; th&#x1eeb;a"/>
+<node CREATED="1539445695555" ID="ID_1792185234" MODIFIED="1539445699992" TEXT="T&#xed;nh &#x111;a h&#xec;nh"/>
+<node CREATED="1539445700243" ID="ID_1497086332" MODIFIED="1539445702378" TEXT="T&#xed;nh &#x111;&#xf3;ng g&#xf3;i"/>
+</node>
+<node CREATED="1539445705960" ID="ID_1153462666" MODIFIED="1539445721853" TEXT="D&#x1eab;n t&#x1edb;i l&#x1eb7;p code, m&#x1ed9;t s&#x1ed1; l&#x1ed7;i ti&#x1ec1;m &#x1ea9;n, thi&#x1ebf;t k&#x1ebf; kh&#xf4;ng ch&#x1eb7;t ch&#x1ebd;"/>
+<node CREATED="1539445737504" ID="ID_1704432557" MODIFIED="1539445741681" TEXT="Design pattern">
+<node CREATED="1539445743363" ID="ID_1856483781" MODIFIED="1539445764660" TEXT="Hi&#x1ec3;u v&#x1ec1; nguy&#xea;n t&#x1eaf;c thi&#x1ebf;t k&#x1ebf;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1539445757883" ID="ID_1351457227" MODIFIED="1539445762160" TEXT="trong vi&#x1ec7;c t&#x1ea1;o class v&#xe0; object"/>
+</node>
+<node CREATED="1539445780728" ID="ID_982532341" MODIFIED="1539445787928" TEXT="Hi&#x1ec3;u nguy&#xea;n l&#xfd; &#x111;&#x1ec3; b&#x1ed5; sung"/>
+<node CREATED="1539445797871" ID="ID_698205268" MODIFIED="1539445805850" TEXT="Ki&#x1ebf;n tr&#xfa;c + thi&#x1ebf;t k&#x1ebf; OOP t&#x1ed1;t h&#x1a1;n"/>
+</node>
+</node>
+<node CREATED="1539445812274" ID="ID_1273125721" MODIFIED="1539445823325" TEXT="Nh&#x1ead;n ra m&#x1eab;u pattern trong th&#x1b0; vi&#x1ec7;n v&#xe0; ng&#xf4;n ng&#x1eef;">
+<node CREATED="1539445838065" ID="ID_1946392195" MODIFIED="1539445850431" TEXT="Design pattern = gi&#x1ea3;i ph&#xe1;p chung cho v&#x1ea5;n &#x111;&#x1ec1; chung trong OOP"/>
+<node CREATED="1539445868981" ID="ID_1423277868" MODIFIED="1539445879766" TEXT="Gi&#x1ea3;i ph&#xe1;p trong v&#x1ea5;n &#x111;&#x1ec1; thi&#x1ebf;t k&#x1ebf; c&#x1ee7;a b&#x1ea3;n th&#xe2;n"/>
+</node>
+</node>
+</node>
+</map>
---
+++ Lap trinh huong doi tuong OOP.mm
@@ -0,0 +1,6 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1539446202525" ID="ID_1143506264" MODIFIED="1539446210760" TEXT="Lap trinh huong doi tuong OOP">
+<node CREATED="1539446687263" ID="ID_332647996" MODIFIED="1539446695858" POSITION="right" TEXT="L&#x1ead;p tr&#xec;nh chia l&#xe0;m c&#xe1;c &#x111;&#x1ed1;i t&#x1b0;&#x1ee3;ng &#x111;&#x1ec3; x&#x1eed; l&#xfd;"/>
+</node>
+</map>
